202c50abc9750731296000b8eccea29e43c98b2c
oleh <olnavr7@gmail.com>
2019-05-06 13:36:34 +0200

    dodanie vgg19 do projektu

---
+++ zadanie_nr1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from keras.applications.vgg19 import VGG19
+
+PATH_TO_IMAGES = 'common-mobile-web-app-icons'
+
+def main(args):
+    model = VGG19()
+    print(model.summary())
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
