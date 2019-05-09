4840c1b8e81abd8369615763c37bba06ec5d1534
Artur Brodzki <artur@brodzki.org>
2019-05-09 11:02:48 +0200

    Build 11:02-09.05.2019

--- zadanie_nr1.py
+++ zadanie_nr1.py
@@ -4,10 +4,12 @@ from keras.applications.vgg19 import VGG19
 
 PATH_TO_IMAGES = 'common-mobile-web-app-icons'
 
+
 def main(args):
     model = VGG19()
     print(model.summary())
     return 0
+
 
 if __name__ == '__main__':
     import sys
