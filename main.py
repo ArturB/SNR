0f70a19365e61fa57a8ce881fd3e4a9be0d0d60d
Artur Brodzki <artur@brodzki.org>
2019-05-10 23:53:45 +0200

    Build 23:53-10.05.2019

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 if __name__ == '__main__':
     data_root     = pathlib.Path("./dataset/_positive")
     data_root_neg = pathlib.Path("./dataset/_negative")
-    all_image_paths = list(data_root.glob('*/*'))
+    all_image_paths = list(data_root.glob('*/*.JPG'))
     all_image_paths = [str(path) for path in all_image_paths]
     random.shuffle(all_image_paths)
 
@@ -21,6 +21,20 @@ if __name__ == '__main__':
     print("Labels (", len(label_names), "): ", label_names)
     label_to_index = dict((name, index) for index, name in enumerate(label_names))
     print("Labels dictionary: ", label_to_index)
+    all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in all_image_paths]
+    print("First 10 labels indices: ", all_image_labels[:10])
+
+    # img_path = all_image_paths[0]
+    # print(img_path)
+    # img_raw = tf.read_file(img_path)
+    # img_tensor = tf.image.decode_image(img_raw)
+    # print(img_tensor.shape)
+    # print(img_tensor.dtype)
+
+    all_image_tensors = [tf.image.decode_image(tf.read_file(img_path)) for img_path in all_image_paths[:10000]]
+    all_200x200_tensors = list(filter(lambda x: x.shape == (200,200,3), all_image_tensors))
+    print(len(all_200x200_tensors))
+
 
 
 
