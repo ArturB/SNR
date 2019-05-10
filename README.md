c9265f4e1898713b24268bd085e99c5f08e2b74f
Artur Brodzki <artur@brodzki.org>
2019-05-10 22:36:55 +0200

    gitignore dataset

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SNR
 Classification of icon images, using convolutional deep neural network. 
+
+You should paste the dataset into main project directory: ./dataset . 
--- main.py
+++ main.py
@@ -1,77 +1,27 @@
 from __future__ import absolute_import, division, print_function
+import IPython.display as display
+import pathlib
+import random
 
-# TensorFlow and tf.keras
 import tensorflow as tf
-from tensorflow import keras
+tf.enable_eager_execution()
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+if __name__ == '__main__':
+    data_root     = pathlib.Path("./dataset/_positive")
+    data_root_neg = pathlib.Path("./dataset/_negative")
+    all_image_paths = list(data_root.glob('*/*'))
+    all_image_paths = [str(path) for path in all_image_paths]
+    random.shuffle(all_image_paths)
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=5)
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+    image_count = len(all_image_paths)
+    print("Image count: ", image_count)
+    label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+    print("Labels (", len(label_names), "): ", label_names)
+    label_to_index = dict((name, index) for index, name in enumerate(label_names))
+    print("Labels dictionary: ", label_to_index)
 
 
-def plot_image(i, predictions_array, true_label, img):
-    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
 
-    plt.imshow(img, cmap=plt.cm.binary)
-
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
-
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                         100 * np.max(predictions_array),
-                                         class_names[true_label]),
-               color=color)
-
-
-def plot_value_array(i, predictions_array, true_label):
-    predictions_array, true_label = predictions_array[i], true_label[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
-
-
-# Plot the first X test images, their predicted label, and the true label
-# Color correct predictions in blue, incorrect predictions in red
-predictions = model.predict(test_images)
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions, test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions, test_labels)
-plt.show()
 
