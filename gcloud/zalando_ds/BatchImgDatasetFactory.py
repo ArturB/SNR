782958dff22fa13f09a6a28e5b4097b3212baba3
Artur Brodzki <artur@brodzki.org>
2019-05-24 01:13:30 +0200

    Build 01:12-24.05.2019

--- gcloud/zalando_ds/BatchImgDatasetFactory.py
+++ gcloud/zalando_ds/BatchImgDatasetFactory.py
@@ -1,8 +1,7 @@
 from __future__ import \
     absolute_import, \
     print_function
-import keras
-import pathlib
+from os import path
 import random
 import tensorflow as tf
 
@@ -45,14 +44,14 @@ class BatchImgDatasetFactory:
             train_images_num,
             file_format=".jpg"
     ):
-        data_root = pathlib.Path(data_root_path)
+        data_root = path.Path(data_root_path)
         all_image_paths = list(data_root.glob('*/*' + file_format))
         all_image_paths = [str(path) for path in all_image_paths]
         random.shuffle(all_image_paths)
 
         label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
         label_to_index = dict((name, index) for index, name in enumerate(label_names))
-        all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in all_image_paths]
+        all_image_labels = [label_to_index[path.Path(path).parent.name] for path in all_image_paths]
 
         train_image_paths = all_image_paths[:train_images_num]
         train_image_labels = all_image_labels[:train_images_num]
@@ -76,7 +75,7 @@ class BatchImgDatasetFactory:
         return train_ds, test_ds, len(label_names)
 
     def zalando_dataset(self):
-        fashion_mnist = keras.datasets.fashion_mnist
+        fashion_mnist = tf.keras.datasets.fashion_mnist
         (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
         def multiplicate_channel(t):
